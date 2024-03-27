817120f0a5349c8bb4e5350e161775aa11d5d909
ElielRegiani <elielregiani@gmail.com>
2024-03-27 15:49:05 -0300

    First Commit

---
+++ pagamentos/src/test/java/br/com/allurafood/pagamentos/PagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allurafood.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
